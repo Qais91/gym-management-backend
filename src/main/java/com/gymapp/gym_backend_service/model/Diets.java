25d9336e4c5445eac34c108244df940454059c00
Qais91 <kais.safdary@gmail.com>
2025-07-22 01:54:46 +0530

    diets and gym session model included

---
+++ src/main/java/com/gymapp/gym_backend_service/model/Diets.java
@@ -0,0 +1,20 @@
+package com.gymapp.gym_backend_service.model;
+
+import jakarta.persistence.*;
+
+public class Diets {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String mealType;
+
+    @Column(length = 1000)
+    private String foodItems;
+
+    private Integer calories;
+
+    @ManyToOne
+    @JoinColumn(name = "custom_diet_plan_id")
+    private CustomDietPlan customDietPlan;
+}
---
+++ src/main/java/com/gymapp/gym_backend_service/model/GymSession.java
@@ -0,0 +1,25 @@
+package com.gymapp.gym_backend_service.model;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+public class GymSession {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDateTime sessionDateTime;
+
+    private int durationInMinutes;
+
+    @Column(length = 1000)
+    private String notes;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne
+    @JoinColumn(name = "trainer_id")
+    private Trainer trainer;
+}

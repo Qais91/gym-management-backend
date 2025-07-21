812b4327e4369e10e3dfa17500f20764c39140b8
Qais91 <kais.safdary@gmail.com>
2025-07-22 01:54:26 +0530

    membership controller and repository created

---
+++ src/main/java/com/gymapp/gym_backend_service/controller/MembershipController.java
@@ -0,0 +1,25 @@
+package com.gymapp.gym_backend_service.controller;
+
+import com.gymapp.gym_backend_service.model.Membership;
+import com.gymapp.gym_backend_service.repository.MembershipRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/memberships")
+public class MembershipController {
+
+    @Autowired
+    private MembershipRepository membershipRepository;
+
+    @PostMapping
+    public Membership create(@RequestBody Membership membership) {
+        return membershipRepository.save(membership);
+    }
+
+    @GetMapping("/{id}")
+    public Membership getById(@PathVariable Long id) {
+        return membershipRepository.findById(id).orElseThrow();
+    }
+
+}
---
+++ src/main/java/com/gymapp/gym_backend_service/repository/MembershipRepository.java
@@ -0,0 +1,9 @@
+package com.gymapp.gym_backend_service.repository;
+
+import com.gymapp.gym_backend_service.model.Membership;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MembershipRepository extends JpaRepository<Membership, Long> {
+}

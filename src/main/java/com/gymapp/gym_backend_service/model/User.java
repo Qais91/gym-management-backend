6fa395df7373965ab3e56cb679ef58eebbd82e29
Qais91 <kais.safdary@gmail.com>
2025-07-23 01:18:19 +0530

    user addition controller implementation

--- src/main/java/com/gymapp/gym_backend_service/model/User.java
+++ src/main/java/com/gymapp/gym_backend_service/model/User.java
@@ -21,4 +21,26 @@ public class User {
 
     private String password;
     private LocalDate joinedDate = LocalDate.now();
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+
+    public String getPhoneNumber() { return phoneNumber; }
+    public void setPhoneNumber(String phoneNumber) { this.phoneNumber = phoneNumber; }
+
+    public LocalDate getJoinedDate() { return joinedDate; }
+    public void setJoinedDate(LocalDate joinedDate) { this.joinedDate = joinedDate; }
+
 }

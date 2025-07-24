0b36d15044587c5262b9e524a5de4aacc7bf8bd1
Qais91 <kais.safdary@gmail.com>
2025-07-25 01:21:52 +0530

    implemented controller to get member or trainer

--- src/main/java/com/gymapp/gym_backend_service/controller/MemberController.java
+++ src/main/java/com/gymapp/gym_backend_service/controller/MemberController.java
@@ -3,19 +3,27 @@ package com.gymapp.gym_backend_service.controller;
 import com.gymapp.gym_backend_service.model.Member;
 import com.gymapp.gym_backend_service.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
-@RequestMapping("/api/members")
+@RequestMapping("/api/member")
 public class MemberController {
 
     @Autowired
     private MemberRepository memberRepository;
 
     @PostMapping
-    public Member createMember(@RequestBody Member member) {
-        return memberRepository.save(member);
+    public ResponseEntity<Member> createMember(@RequestBody Member member) {
+        Member savedMember = memberRepository.save(member);
+        return ResponseEntity.ok(savedMember);
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Member> getCustomerById(@PathVariable Long id) {
+        return memberRepository.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
 }
--- src/main/java/com/gymapp/gym_backend_service/controller/TrainerController.java
+++ src/main/java/com/gymapp/gym_backend_service/controller/TrainerController.java
@@ -3,10 +3,11 @@ package com.gymapp.gym_backend_service.controller;
 import com.gymapp.gym_backend_service.model.Trainer;
 import com.gymapp.gym_backend_service.repository.TrainerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/trainers")
+@RequestMapping("/api/trainer")
 public class TrainerController {
 
     @Autowired
@@ -15,5 +16,12 @@ public class TrainerController {
     @PostMapping
     public Trainer addTrainer(@RequestBody Trainer trainer) {
         return trainerRepository.save(trainer);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Trainer> getTrainerById(@PathVariable Long id) {
+        return trainerRepository.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
     }
 }

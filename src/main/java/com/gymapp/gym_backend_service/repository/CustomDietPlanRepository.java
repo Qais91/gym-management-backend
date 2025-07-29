0abbd3e09cab95552931a28b57e971a10d5d48b1
Qais91 <kais.safdary@gmail.com>
2025-07-29 23:39:05 +0530

    respository udpated for custom diet plan

--- src/main/java/com/gymapp/gym_backend_service/repository/CustomDietPlanRepository.java
+++ src/main/java/com/gymapp/gym_backend_service/repository/CustomDietPlanRepository.java
@@ -3,5 +3,8 @@ package com.gymapp.gym_backend_service.repository;
 import com.gymapp.gym_backend_service.model.CustomDietPlan;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CustomDietPlanRepository extends JpaRepository<CustomDietPlan, Long> {
+    List<CustomDietPlan> findByMemberId(Long memberId);
 }

75e122869824f05d739c0720e7d7f5e5e65961c3
Qais91 <kais.safdary@gmail.com>
2025-08-11 02:05:10 +0530

    gym session model updated

--- src/main/java/com/gymapp/gym_backend_service/model/GymSession.java
+++ src/main/java/com/gymapp/gym_backend_service/model/GymSession.java
@@ -1,20 +1,15 @@
 package com.gymapp.gym_backend_service.model;
 
+import com.gymapp.gym_backend_service.model.enums.ActivityType;
 import jakarta.persistence.*;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
 public class GymSession {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    private LocalDateTime sessionDateTime;
-
-    private int durationInMinutes;
-
-    @Column(length = 1000)
-    private String notes;
 
     @ManyToOne
     @JoinColumn(name = "member_id")
@@ -23,4 +18,34 @@ public class GymSession {
     @ManyToOne
     @JoinColumn(name = "trainer_id")
     private Trainer trainer;
+
+
+    @Enumerated(EnumType.STRING)
+    private ActivityType activityType;
+    @Column(length = 1000)
+    private String notes;
+
+    private LocalDateTime sessionStartDateTime;
+    private LocalDateTime sessionEndDateTime;
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public Member getMember() { return member; }
+    public void setMember(Member member) { this.member = member; }
+
+    public Trainer getTrainer() { return trainer; }
+    public void setTrainer(Trainer trainer) { this.trainer = trainer; }
+
+    public ActivityType getActivityType() { return activityType; }
+    public void setActivityType(ActivityType activityType) { this.activityType = activityType; }
+
+    public String getNotes() { return notes; }
+    public void setNotes(String notes) { this.notes = notes; }
+
+    public LocalDateTime getStartTime() { return sessionStartDateTime; }
+    public void setStartTime(LocalDateTime startTime) { this.sessionStartDateTime = startTime; }
+
+    public LocalDateTime getEndTime() { return sessionEndDateTime; }
+    public void setEndTime(LocalDateTime endTime) { this.sessionEndDateTime = endTime; }
 }

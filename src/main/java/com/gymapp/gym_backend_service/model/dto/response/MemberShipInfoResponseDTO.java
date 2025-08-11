86eb3eee6bb0ff4c0742a4eb66f73b67731983d3
Qais91 <kais.safdary@gmail.com>
2025-08-12 01:57:28 +0530

    membership response DTO

---
+++ src/main/java/com/gymapp/gym_backend_service/model/dto/response/MemberShipInfoResponseDTO.java
@@ -0,0 +1,19 @@
+package com.gymapp.gym_backend_service.model.dto.response;
+
+import com.gymapp.gym_backend_service.model.Membership;
+
+public class MemberShipInfoResponseDTO {
+    private Long membershipID;
+    private String planName;
+    private double price;
+
+    public MemberShipInfoResponseDTO(Membership membership) {
+        membershipID = membership.getId();
+        planName = membership.getTitle();
+        price = membership.getPricePerDurationMonth();
+    }
+
+    public Long getMembershipID() { return membershipID; }
+    public String getPlanName() { return planName; }
+    public double getPrice() { return price; }
+}

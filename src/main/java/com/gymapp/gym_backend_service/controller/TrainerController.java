913b92c98222457311bbaccdffa3dddd3ffb00af
Qais91 <kais.safdary@gmail.com>
2025-08-12 01:56:09 +0530

    trainer controller update with response messages

--- src/main/java/com/gymapp/gym_backend_service/controller/TrainerController.java
+++ src/main/java/com/gymapp/gym_backend_service/controller/TrainerController.java
@@ -1,12 +1,14 @@
 package com.gymapp.gym_backend_service.controller;
 
 import com.gymapp.gym_backend_service.model.Trainer;
+import com.gymapp.gym_backend_service.model.User;
 import com.gymapp.gym_backend_service.model.dto.response.ApiResponse;
 import com.gymapp.gym_backend_service.model.dto.response.UserResponse;
 import com.gymapp.gym_backend_service.model.enums.UserRole;
 import com.gymapp.gym_backend_service.repository.TrainerRepository;
 import com.gymapp.gym_backend_service.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,7 +25,7 @@ public class TrainerController {
 
     @PostMapping
     public ResponseEntity<?> addTrainer(@RequestBody Trainer trainer) {
-        if (userRepository.existsByUsername(trainer.getName())) {
+        if (userRepository.existsByUsername(trainer.getUsername())) {
             return ResponseEntity.badRequest().body(new ApiResponse("false", "Username already exists"));
         }
         if(userRepository.existsByEmail(trainer.getEmail())) {
@@ -36,10 +38,17 @@ public class TrainerController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Trainer> getTrainerById(@PathVariable Long id) {
-        return trainerRepository.findById(id)
-                .map(ResponseEntity::ok)
-                .orElse(ResponseEntity.notFound().build());
+    public ResponseEntity<?> getTrainerById(@PathVariable Long id) {
+        List<User> trainer = userRepository.findByUserRole(UserRole.Trainer);
+        if(trainer.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(new ApiResponse("error", "No Members"));
+        }
+        return ResponseEntity.ok(
+                trainer.stream()
+                        .map((user) -> new UserResponse(user))
+                        .toList()
+        );
     }
 
     @GetMapping

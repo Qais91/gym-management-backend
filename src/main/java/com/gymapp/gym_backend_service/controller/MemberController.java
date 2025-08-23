c620ef9fe046f3c8246910f6b82d6d0ffe4ae564
Qais91 <kais.safdary@gmail.com>
2025-08-23 16:35:38 +0530

    Member creation with authorized role impementation

--- src/main/java/com/gymapp/gym_backend_service/controller/MemberController.java
+++ src/main/java/com/gymapp/gym_backend_service/controller/MemberController.java
@@ -1,20 +1,29 @@
 package com.gymapp.gym_backend_service.controller;
 
 import com.gymapp.gym_backend_service.model.Member;
+import com.gymapp.gym_backend_service.model.Trainer;
 import com.gymapp.gym_backend_service.model.User;
+import com.gymapp.gym_backend_service.model.dto.request.member.CreateMemberRequestDTO;
 import com.gymapp.gym_backend_service.model.dto.response.ApiResponse;
 import com.gymapp.gym_backend_service.model.dto.response.UserResponse;
 import com.gymapp.gym_backend_service.model.enums.UserRole;
 import com.gymapp.gym_backend_service.repository.MemberRepository;
+import com.gymapp.gym_backend_service.repository.TrainerRepository;
 import com.gymapp.gym_backend_service.repository.UserRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
+import javax.swing.text.html.Option;
 import java.util.List;
 import java.util.Optional;
 
+@EnableMethodSecurity
 @RestController
 @RequestMapping("/api/member")
 public class MemberController {
@@ -23,9 +32,12 @@ public class MemberController {
     private MemberRepository memberRepository;
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private TrainerRepository trainerRepo;
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping
-    public ResponseEntity<?> createMember(@RequestBody Member member) {
+    public ResponseEntity<?> createMember(@Valid @RequestBody CreateMemberRequestDTO member) {
         if (userRepository.existsByUsername(member.getName())) {
             return ResponseEntity.badRequest().body(new ApiResponse("false", "Username already exists"));
         }
@@ -36,7 +48,19 @@ public class MemberController {
             return ResponseEntity.badRequest().body(new ApiResponse("false", "Number already exists"));
         }
 
-        Member savedMember = memberRepository.save(member);
+        Optional<Trainer> trainer = Optional.empty();
+        if(member.getTrainerID() != null) {
+            trainer = trainerRepo.findById(member.getTrainerID());
+            if(trainer.isEmpty())  return ResponseEntity.badRequest().body(new ApiResponse("false", "Enter a valid user ID"));
+        }
+
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+        Member mem = new Member(member);
+        mem.setPassword(encoder.encode(member.getPassword()));
+        if(!trainer.isEmpty())  mem.setTrainer(trainer.get());
+
+        Member savedMember = memberRepository.save(mem);
         return ResponseEntity.ok(savedMember);
     }
 
@@ -51,9 +75,10 @@ public class MemberController {
         return ResponseEntity.ok(memberOpt.get());
     }
 
+    @PreAuthorize("hasRole('ADMIN', 'TRAINER')")
     @GetMapping
     public ResponseEntity<?> getAllMemeber() {
-        List<User> members = userRepository.findByUserRole(UserRole.GymMemeber);
+        List<User> members = userRepository.findByUserRole(UserRole.MEMBER);
         if(members.isEmpty()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND)
                     .body(new ApiResponse("error", "No Members"));
--- src/main/java/com/gymapp/gym_backend_service/model/Member.java
+++ src/main/java/com/gymapp/gym_backend_service/model/Member.java
@@ -1,14 +1,22 @@
 package com.gymapp.gym_backend_service.model;
 
+import com.gymapp.gym_backend_service.model.dto.request.member.CreateMemberRequestDTO;
 import com.gymapp.gym_backend_service.model.enums.UserRole;
 import jakarta.persistence.*;
-import java.time.LocalDate;
 
 @Entity
 @PrimaryKeyJoinColumn(name = "id")
 public class Member extends User {
 
-    Member() { setUserRole(UserRole.GymMemeber); }
+    Member() { setUserRole(UserRole.MEMBER); }
+
+    public Member(CreateMemberRequestDTO memberInfo) {
+        setName(memberInfo.getName());
+        setUsername(memberInfo.getUsername());
+        setEmail(memberInfo.getEmail());
+        setPhoneNumber(memberInfo.getPhoneNumber());
+        setUserRole(UserRole.MEMBER);
+    }
 
     @ManyToOne
     @JoinColumn(name = "personal_trainer_id")

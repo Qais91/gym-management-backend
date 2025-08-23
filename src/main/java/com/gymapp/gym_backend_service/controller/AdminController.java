fb4b490b9ea3a851076c99e5aaf0962a9151493c
Qais91 <kais.safdary@gmail.com>
2025-08-23 16:35:56 +0530

    admin controller implemented

---
+++ src/main/java/com/gymapp/gym_backend_service/controller/AdminController.java
@@ -0,0 +1,32 @@
+package com.gymapp.gym_backend_service.controller;
+
+import com.gymapp.gym_backend_service.model.User;
+import com.gymapp.gym_backend_service.model.enums.UserRole;
+import com.gymapp.gym_backend_service.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.gymapp.gym_backend_service.model.User;
+import com.gymapp.gym_backend_service.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/admin")
+public class AdminController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping
+    public User createAdmin(@RequestBody User user) {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        user.setUserRole(UserRole.ADMIN);
+        user.setPassword(encoder.encode(user.getPassword()));
+        return userRepository.save(user);
+    }
+}

dc838b2531dc7169d2585ab0a6fa914f3284c174
Qais91 <kais.safdary@gmail.com>
2025-08-23 16:21:27 +0530

    registered membership segregated

--- src/main/java/com/gymapp/gym_backend_service/model/dto/response/registered_membership/RegisteredMembershipInfoResponseDTO.java
+++ src/main/java/com/gymapp/gym_backend_service/model/dto/response/registered_membership/RegisteredMembershipInfoResponseDTO.java
@@ -1,46 +1,26 @@
 package com.gymapp.gym_backend_service.model.dto.response.registered_membership;
 
-import com.gymapp.gym_backend_service.model.CustomDietPlan;
-import com.gymapp.gym_backend_service.model.RegisteredMemberships;
+import com.gymapp.gym_backend_service.model.RegisteredMembership;
 import com.gymapp.gym_backend_service.model.dto.response.custom_diet_plan.DietPlanResponseDTO;
-import com.gymapp.gym_backend_service.model.enums.RegistrationStatus;
 
 import java.time.LocalDate;
-import java.util.List;
 
-public class RegisteredMembershipInfoResponseDTO {
+public class RegisteredMembershipInfoResponseDTO extends RegMembershipOverviewInfoDTO {
 
-    private Long id;
-    private String memberName;
-    private String planName;
-    private int memberShipValidDuration;
-    private RegistrationStatus status;
-    private String validator;
     private String uploadedDoc;
     private LocalDate endDate;
     private DietPlanResponseDTO customdietPlan;
 
-    public RegisteredMembershipInfoResponseDTO(RegisteredMemberships regMeberShip) {
-        id = regMeberShip.getId();
-        memberName = regMeberShip.getMember().getName();
-        planName = regMeberShip.getMembership().getTitle();
-        memberShipValidDuration = regMeberShip.getMembership().getDurationInMonths();
-        status = regMeberShip.getStatus();
-        validator = (regMeberShip.getValidator() != null) ? regMeberShip.getValidator().getName() : "-";
-        endDate = regMeberShip.getEndDate();
-        customdietPlan = new DietPlanResponseDTO(regMeberShip.getDietPlan());
+    public RegisteredMembershipInfoResponseDTO(RegisteredMembership regMemberShip) {
+        super(regMemberShip);
+        endDate = regMemberShip.getEndDate();
+        customdietPlan = new DietPlanResponseDTO(regMemberShip.getDietPlan());
         uploadedDoc = "-";
     }
 
     public void setUploadedDoc(String docPath) { uploadedDoc = docPath; }
 
-    public Long getId() { return id; }
-    public String getPlanName() { return planName; }
-    public String getMemberName() { return memberName; }
-    public String getValidator() { return validator; }
-    public int getMemberShipValidDuration() { return memberShipValidDuration; }
     public String getUploadedDoc() { return uploadedDoc; }
-    public RegistrationStatus getStatus() { return status; }
     public DietPlanResponseDTO getCustomdietPlan() { return customdietPlan; }
     public LocalDate getEndDate() { return endDate; }
 }

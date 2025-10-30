6fc1965c32147e100e3058b53b7a80dea36956f0
Qais91 <kais.safdary@gmail.com>
2025-10-30 22:48:30 +0530

    request DTO constructor changes or setter changes

--- src/main/java/com/gymapp/gym_backend_service/data/dto/request/member/CreateMemberRequestDTO.java
+++ src/main/java/com/gymapp/gym_backend_service/data/dto/request/member/CreateMemberRequestDTO.java
@@ -1,9 +1,6 @@
 package com.gymapp.gym_backend_service.data.dto.request.member;
 
 import com.gymapp.gym_backend_service.data.dto.request.CreateUserRequestDTO;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 public class CreateMemberRequestDTO extends CreateUserRequestDTO {
 
